08282fb38caf9a6328a439d71bf91bac8ea51fc0
Tarun <prht.tarun@gmail.com>
2025-08-16 02:03:05 +0530

    init commit

--- src/main/java/com/app/todo/entity/Task.java
+++ src/main/java/com/app/todo/entity/Task.java
@@ -13,7 +13,7 @@ import lombok.Setter;
 public class Task {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String title;
     private boolean status;
